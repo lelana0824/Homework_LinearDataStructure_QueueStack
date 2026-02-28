c64bdab46c8e5d2e4de799de6d026b8e918f7dc3
lelana0824 <lelana0824@gmail.com>
2026-02-28 17:06:33 +0900

    Queue 클래스 구현

--- Container/Queue.h
+++ Container/Queue.h
@@ -6,7 +6,61 @@
 #define LINEARDATASTRUCTURE_QUEUESTACK_QUEUE_H
 
 
+template <typename T, size_t inSize = 512>
 class Queue {
+public:
+    Queue()
+        :front(0), rear(0), size(inSize + 1)
+    {};
+    ~Queue(){};
+
+    bool Enqueue(T inData) {
+        if (IsFull()) return false;
+
+        data[rear] = inData;
+        rear = (rear + 1) % size;
+
+        return true;
+    }
+
+    bool Enqueue(T* inData) {
+        if (IsFull()) return false;
+
+        data[rear] = inData;
+        rear = (rear + 1) % size;
+
+        return true;
+    }
+
+    T Dequeue() {
+        T rData = data[front];
+        data[front] = T();
+
+        front = (front + 1) % size;
+        return rData;
+    }
+
+    bool IsEmpty() const {
+        return front == rear;
+    }
+
+    bool IsFull() const {
+        return (rear + 1) % size == front;
+    }
+
+    T Peek() {
+        return data[front];
+    }
+
+    int Size() const {
+        return inSize;
+    }
+private:
+    int front;
+    int rear;
+    int size;
+
+    T data[inSize + 1];
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include "Container/Stack.h"
+#include "Container/Queue.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    struct Test {
-    public:
-        explicit Test(const int i) {
-            data = i;
-        }
-    private:
-        int data = 0;
-    };
+    /*
+    - 레이저 블랙위도우 TE 크로마 V2동작
+        - ‘A’, ‘B’, ‘C’, ‘D’, ‘E’ 키를 입력 받기
+        - 들어온 입력을 입력 Queue에 넣기
+        - 입력 Queue에 들어온 순서대로 Undo 스택에 기록
+        - “undo” 명령어를 입력하면 undo 실행
+        - “redo” 명령어를 입력하면 redo 실행
+        - “show” 명령어를 입력하면 undo 스택과 redo 스택에 저장된 명령 내용 출력
+     */
 
-    Stack<Test*> stack;
-    Stack<int> intStack;
+    // 키 및 명령어 입력 정의 -> 문자열 입력 후 '엔터'로 정의한다.
+    // 명령어가 아닌 키는 모두 queue에 넣는다.
+    // queue에 들어가는 키는 모두 undo 스택에도 넣는다.
 
-    stack.Push(new Test(3));
-    stack.Pop();
+    // 명령어 입력 시
+    // undo:
+    // 1. undo 스택에서 pop 한 내용을 출력한 후, redo 스택에 넣는다.
+    // redo
+    // 1. redo 스택에서 pop한 내용을 출력한 후, undo 스택에 넣는다.
+    // * 중요 - 키 입력 (명령어가 아닌 모든 입력)시 redo 스택은 초기화된다.
 
-    intStack.Push(3);
-    intStack.Pop();
+    // todo: 스택 초기화 메서드, Show 메서드.
+
 
 
 
